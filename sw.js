a80b5b523b1c5fe012b1e03e28927131d6df4d47
root <root@localhost.localdomain>
2023-07-18 20:05:24 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230714.195250';
+const cacheName = 'chirpy-20230718.200330';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
