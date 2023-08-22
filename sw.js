bff2fe0f7184fb49f61acbc52c5b69196a58c759
root <root@localhost.localdomain>
2023-08-22 12:32:54 +0800

    remove nodes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230803.141524';
+const cacheName = 'chirpy-20230822.123120';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
